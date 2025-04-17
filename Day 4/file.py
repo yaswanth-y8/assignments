50d19551398c5f18b0191906002e014b04e5dfa0
unknown <ysaiyaswanth8@gmail.com>
2025-04-17 12:25:01 +0530

    added folder 4-2

--- Day 4/file.py
+++ Day 4/file.py
@@ -7,15 +7,15 @@ class File:
         self.directory = directory
 
     def getMaxSizeFile(self, n):
-        files_with_size = []
+        files_size = []
         for filename in os.listdir(self.directory):
             file_path = os.path.join(self.directory, filename)
             if os.path.isfile(file_path):
                 file_size = os.path.getsize(file_path)
-                files_with_size.append((filename, file_size))
+                files_size.append((filename, file_size))
                 
-        files_with_size.sort(key=lambda x: x[1], reverse=True)
-        return [file[0] for file in files_with_size[:n]]
+        files_size.sort(key=lambda x: x[1], reverse=True)
+        return [file[0] for file in files_size[:n]]
         
     def getLatestFiles(self, date):
         
@@ -23,9 +23,9 @@ class File:
         for filename in os.listdir(self.directory):
             file_path = os.path.join(self.directory, filename)
             if os.path.isfile(file_path):
-                file_modified_time = os.path.getmtime(file_path)
-                file_modified_date = date.fromtimestamp(file_modified_time)
-                if file_modified_date > date:
+                file_time = os.path.getmtime(file_path)
+                file_date = date.fromtimestamp(file_time)
+                if file_date > date:
                     latest_files.append(filename)
 
         return latest_files    
