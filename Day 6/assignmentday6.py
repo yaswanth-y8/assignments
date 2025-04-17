6c5e376a61269313e9c01ef9cffef9c46e9023b6
unknown <ysaiyaswanth8@gmail.com>
2025-04-17 14:38:51 +0530

    added folder 6

---
+++ Day 6/assignmentday6.py
@@ -0,0 +1,37 @@
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+async def page(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+def ext_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = []
+    
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('http'):  
+            links.append(href)
+    return links
+
+
+
+
+async def dld_links(url):
+    async with aiohttp.ClientSession() as session:
+        
+        html = await page(session, url)
+        links = ext_links(html)
+        print(f"Found {len(links)} links: {links}")
+
+        
+
+
+
+if __name__ == '__main__':
+    url = 'https://www.w3schools.com/tags/ref_colornames.asp' 
+    asyncio.run(dld_links(url))
